1c1dc8238337d2cdd50740f692bf71df1fd7da4f
driade-de <hermesantares19@gmail.com>
2025-12-03 01:13:44 -0600

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,4 +5,3 @@ const supabaseUrl = "https://ophicpcbgdgzthjamzoz.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waGljcGNiZ2RnenRoamFtem96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MjgwMzUsImV4cCI6MjA4MDMwNDAzNX0.K55Tk-cdbAFDOtGrIMN7vYFaJAccrsD6UdOrARt1P5c";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
