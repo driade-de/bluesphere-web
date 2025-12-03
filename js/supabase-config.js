87c2180b837a4df9995df963958d19300b374035
driade-de <hermesantares19@gmail.com>
2025-12-03 01:07:00 -0600

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,8 @@
-js/supabase-config.js
-// Configuración inicial de Supabase para BlueSphere
-const SUPABASE_URL = "";         // Aquí irá tu URL de Supabase
-const SUPABASE_KEY = "";         // Aquí irá tu API Key pública
+// supabase-config.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// Inicializar cliente Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseUrl = "https://ophicpcbgdgzthjamzoz.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waGljcGNiZ2RnenRoamFtem96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MjgwMzUsImV4cCI6MjA4MDMwNDAzNX0.K55Tk-cdbAFDOtGrIMN7vYFaJAccrsD6UdOrARt1P5c";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
